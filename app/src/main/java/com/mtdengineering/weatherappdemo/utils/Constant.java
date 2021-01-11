b096b68abb4561162d392ac0469c016fe0853ac0
Ben Mutondo <ben_mutondo@hotmail.com>
2021-01-11 14:35:24 +0200

    App constants

---
+++ app/src/main/java/com/mtdengineering/weatherappdemo/utils/Constant.java
@@ -0,0 +1,7 @@
+package com.mtdengineering.weatherappdemo.utils;
+
+public class Constant
+{
+    public static String API_BASE_URL = "http://api.openweathermap.org/data/2.5/";
+    public static String API_KEY = "53f9d8e4213222cf517d86dc406d67fc";
+}
